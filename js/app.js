30116082bbf5a436c2c4f30e39a0cd1bba223805
AyanoCyberGod <ayanocyber.god@gmail.com>
2022-06-24 21:00:31 +0500

    Port Commit

---
+++ js/app.js
@@ -0,0 +1,166 @@
+$(function () {
+  const worksSlider = $('[data-slider="slick"]');
+
+  /* Filter
+    =====================*/
+  let filter = $("[data-filter]");
+
+  filter.on("click", function (event) {
+    event.preventDefault();
+
+    let cat = $(this).data("filter");
+
+    if (cat == "all") {
+      $("[data-cat]").removeClass("hide");
+    } else {
+      $("[data-cat]").each(function () {
+        let workCat = $(this).data("cat");
+
+        if (workCat != cat) {
+          $(this).addClass("hide");
+        } else {
+          $(this).removeClass("hide");
+        }
+      });
+    }
+  });
+
+  /* Modal
+    =====================*/
+
+  const modalCall = $("[data-modal]");
+  const modalClose = $("[data-close]");
+
+  modalCall.on("click", function (event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalId = $this.data("modal");
+
+    $(modalId).addClass("show");
+    $("body").addClass("no-scroll");
+
+    setTimeout(function () {
+      $(modalId).find(".modal_dialog").css({
+        transform: "scale(1)",
+      });
+    }, 200);
+
+    worksSlider.slick("setPosition");
+  });
+
+  modalClose.on("click", function (event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalParent = $this.parents(".modal");
+
+    modalParent.find(".modal_dialog").css({
+      transform: "scale(0)",
+    });
+
+    setTimeout(function () {
+      modalParent.removeClass("show");
+      $("body").removeClass("no-scroll");
+    }, 200);
+  });
+
+  $(".modal").on("click", function (event) {
+    let $this = $(this);
+
+    $this.find(".modal_dialog").css({
+      transform: "scale(0)",
+    });
+
+    setTimeout(function () {
+      $this.removeClass("show");
+      $("body").removeClass("no-scroll");
+    }, 200);
+  });
+
+  $(".modal_dialog").on("click", function (event) {
+    event.stopPropagation();
+  });
+
+  /* Slider: https://kenwheeler.github.io/slick/
+    =====================*/
+
+  $('[data-slider="slick"]').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    dots: true,
+  });
+
+  $(".slickPrev").on("click", function (event) {
+    event.preventDefault();
+
+    let currentSlider = $(this).parents(".modal").find('[data-slider="slick"]');
+
+    currentSlider.slick("slickPrev");
+  });
+
+  $(".slickNext").on("click", function (event) {
+    event.preventDefault();
+
+    let currentSlider = $(this).parents(".modal").find('[data-slider="slick"]');
+
+    currentSlider.slick("slickNext");
+  });
+
+  /* Fixed Header
+    =====================*/
+  let header = $("#header");
+  let intro = $("#intro");
+  let introH = intro.innerHeight();
+  let scrollPos = $(window).scrollTop();
+
+  checkScroll(scrollPos, introH);
+
+  $(window).on("scroll resize", function () {
+    introH = intro.innerHeight();
+    scrollPos = $(this).scrollTop();
+
+    checkScroll(scrollPos, introH);
+  });
+
+  function checkScroll(scrollPos, introH) {
+    if (scrollPos > introH) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+  }
+
+  /* Smooth scroll
+    =====================*/
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+
+    let elementId = $(this).data("scroll");
+    let elementOffset = $(elementId).offset().top;
+
+    nav.removeClass("show");
+
+    $("html, body").animate(
+      {
+        scrollTop: elementOffset - 70,
+      },
+      800
+    );
+  });
+
+  /* Mobile nav
+    ==================*/
+
+  const navToggle = $("#navToggle");
+  const nav = $("#nav");
+
+  navToggle.on("click", function (event) {
+    event.preventDefault();
+
+    nav.toggleClass("show");
+  });
+});
